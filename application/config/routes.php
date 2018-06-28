cf1b9435de1129d1cdf9acd6dea99b6c46ea239e
sg.andreev <chemic@abv.bg>
2018-06-29 00:13:13 +0300

    Add news reading pages

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,7 +52,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 
-$route['default_controller'] = 'pages/view';
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
-$route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
+$route['default_controller'] = 'pages/view';
