3e2066522718d3f2ce3840e879efe428a6d1c688
sg.andreev <chemic@abv.bg>
2018-06-28 23:47:19 +0300

    Make controller, home and about page!

---
+++ application/views/pages/about.php
@@ -0,0 +1,7 @@
+<div>
+  This is th first test of this PHP framework using by Chemic Co.
+</div>
+
+<div>
+  This is the save way to make good code here.
+</div>
---
+++ application/views/templates/footer.php
@@ -0,0 +1,4 @@
+<div>&nbsp;</div>
+<em>&copy; Chemic Co. 2018</em>
+       </body>
+</html>
